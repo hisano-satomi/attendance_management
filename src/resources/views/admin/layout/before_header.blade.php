c3e06e3f39b0235456ada048d23e5eb84da6aeb5
hisano satomi <stm1014@yahoo.ne.jp>
2025-09-18 21:36:39 +0900

    .Identifierこの拡張子のファイルを削除

---
+++ src/resources/views/admin/layout/before_header.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>coachtech 勤怠管理アプリ</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
+</head>
+<body>
+    
+</body>
+</html>
